ee66af8384ed141c4d0d6e454a8471224aab10ee
Adityansh Srivastava <adisrivastava027@gmail.com>
2025-12-09 05:31:07 +0000

    added upto phase 4 in day 2

--- basic/d2.js
+++ basic/d2.js
@@ -1,20 +1,33 @@
+//phase 1
+  // let a= 12 cant be redeclared
+  // const b= 13 cant be redeclared, reassigned
+  // var c= 14
+  // d= 15
 
-  let a= 12
-  const b= 13
-  var c= 14
-  d= 15
+  // console.log(a, b, c, d)
+  // a= "twelve"
+  // c= "fourteen"
+  // d= "fifteen"
 
-  console.log(a, b, c, d)
-  a= "twelve"
-  b= "thirteen"
-  c= "fourteen"
-  d= "fifteen"
+  // console.log(a, b, c, d)
 
-  console.log(a, b, c, d)
+  // var c= 14
+  // d= 15
 
-  const a= 12
-  let b= 13
-  var c= 14
-  d= 15
+  // console.log(a, b, c, d)
 
-  console.log(a, b, c, d)
+//phase 2
+  //  let a= "abc"
+  //  let b= ""
+  //  let c= null
+  //  let d
+
+  //   console.log(a, b, c, d)
+
+//phase 3
+    let score= "33sbc"
+    console.log(typeof(score), typeof score, score) 
+    let num= Number(score)
+    console.log(typeof(num), typeof num, num) 
+
+//phase 4: datetime, timestamp
