e37db9efbb085fd405e061787367e4b2c2e78ec9
Adityansh Srivastava <adisrivastava027@gmail.com>
2025-12-11 05:38:03 +0000

    day for done

--- basic/d2.js
+++ basic/d2.js
@@ -31,3 +31,19 @@
     // console.log(typeof(num), typeof num, num) 
 
 //phase 4: datetime, timestamp
+md= new Date()
+console.log(md)
+console.log(md.toString())
+console.log(md.toISOString())
+console.log(md.toDateString())
+console.log(md.toTimeString())
+console.log(md.getTime()) //timestamp
+console.log(md.getFullYear())
+console.log(md.getMonth()+1) //0-11
+console.log(md.getDate())
+console.log(md.getHours())
+console.log(md.getMinutes())
+console.log(md.getSeconds())
+console.log(md.getMilliseconds())
+console.log(md.getDay()) //0-6
+// 0: Sunday, 1: Monday, ..., 6: Saturday
